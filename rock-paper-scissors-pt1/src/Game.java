3a84365c5bca02db8828905f511d5faf5f4c6147
Dorron <dorron.green@gmail.com>
2025-05-08 16:26:37 -0400

    rock paper scissor v2 - completed code refactoring.

--- rock-paper-scissors-pt1/src/Game.java
+++ rock-paper-scissors-pt1/src/Game.java
@@ -8,82 +8,133 @@ public class Game {
     3. scissors beats paper
      */
 
-    public static void main(String[] args) {
+    // "static" every level of game can access this Scanner variable.
+    static Scanner console = new Scanner(System.in);
 
-        // we will take an input from the user - user choice
-        // variables
-        Scanner console = new Scanner(System.in);
-        String userChoice;
+
+
+    public static void main(String[] args) {
         boolean exit = false;
 
+        //Human Vs computer
         do {
-            // variable for our validChoice
+            int userMove = getUserChoice();
+            int computerMove = computerChoice();
+            String userChoice = generateMove(userMove);
+            String computerChoice = generateMove(computerMove);
+            determineWinner(userChoice,computerChoice);
+            exit = playAgain();
+        } while (!exit);
+
+//        //Human Vs Human
+//        do {
+//            int userOneMove = getUserChoice();
+//            int userTwoMove = getUserChoice();
+//            String userOneChoice = generateMove(userOneMove);
+//            String userTwoChoice = generateMove(userTwoMove);
+//            determineWinner(userOneChoice,userTwoChoice);
+//            exit = playAgain();
+//        }while(!exit);
+///
+        //Computer vs Computer
+//        int computerOneMove = computerChoice();
+//        int computerTwoMove = computerChoice();
+//        String computerOneChoice = generateMove(computerOneMove);
+//        String computerTwoChoice = generateMove(computerTwoMove);
+//        determineWinner(computerOneChoice,computerTwoChoice);
+
+
+
+
+    }  // this closes the main method
+
+
+
+
+    // Refactor our game
+    // grab the user choice
+        public static int getUserChoice() {
+            // variable for our Choices
             boolean validChoice = false;
+            int userChoice;
             // Get User Choice
             do {
                 System.out.println("Please select your move [rock, paper, or scissors]:");
-                userChoice = console.nextLine();
-                if (userChoice.startsWith("rock") || userChoice.equalsIgnoreCase("paper") || userChoice.equalsIgnoreCase("scissors")) {
+                System.out.println("0. Rock");
+                System.out.println("1. Paper");
+                System.out.println("2. Scissors");
+                userChoice = Integer.parseInt(console.nextLine());
+                if (userChoice == 0 || userChoice == 1 || userChoice == 2) {
                     validChoice = true;
                 } else {
                     System.out.println("Error: invalid choice");
                 }
             } while (!validChoice); // the loop will run as long as validChoice remains false
+            return userChoice;
+        }
 
-
-            // randomly generate a choice - computer choice
+        // grab the computer choice
+        public static int computerChoice () {
             int random = (int) Math.floor(Math.random() * 3);
-            String computerChoice = null;
+            return random;
+        }
 
-            // compare the choices and determine a winner
-            switch (random) {
+        //generate a move
+        public static String generateMove (int playerChoice){
+
+            String move = null;
+            switch (playerChoice) {
                 case 0:
-                    computerChoice = "rock";
+                    move = "rock";
                     break;
                 case 1:
-                    computerChoice = "paper";
+                    move = "paper";
                     break;
                 case 2:
-                    computerChoice = "scissors";
+                    move = "scissors";
                     break;
                 default:// this will never happen
-                    computerChoice = null;
+                    move = null;
             }
-            System.out.println(computerChoice);
-            // compare the choices and determine a winner
+            return move;
+        }
 
+        // determine a winner
+        // Accepts 2 parameters to compare strings userOneChoice vs userTwoChoice
+        public static void determineWinner (String userOneChoice, String userTwoChoice){
             String graphic = "L O A D I N G";
             for (int i = 0; i < graphic.length(); i++) {
                 System.out.println(graphic.charAt(i));
             }
 
-            if (computerChoice.equalsIgnoreCase(userChoice)) {
-                System.out.printf("You both picked %s, it's a tie!%n", userChoice);
-            } else if (computerChoice.equalsIgnoreCase("rock") && userChoice.equalsIgnoreCase("paper")) {
-                System.out.printf("The computer picked %s and you picked %s - You win!! Yay!", computerChoice, userChoice);
-            } else if (computerChoice.equalsIgnoreCase("paper") && userChoice.equalsIgnoreCase("scissors")) {
-                System.out.printf("The computer picked %s and you picked %s - You win!! Yay!", computerChoice, userChoice);
-            } else if (computerChoice.equalsIgnoreCase("scissors") && userChoice.equalsIgnoreCase("rock")) {
-                System.out.printf("The computer picked %s and you picked %s - You win!! Yay!", computerChoice, userChoice);
+            if (userTwoChoice.equalsIgnoreCase(userOneChoice)) {
+                System.out.printf("You both picked %s, it's a tie!%n", userOneChoice);
+            } else if (userTwoChoice.equalsIgnoreCase("rock") && userOneChoice.equalsIgnoreCase("paper")) {
+                System.out.printf("The computer picked %s and you picked %s - You win!! Yay!", userTwoChoice, userOneChoice);
+            } else if (userTwoChoice.equalsIgnoreCase("paper") && userOneChoice.equalsIgnoreCase("scissors")) {
+                System.out.printf("The computer picked %s and you picked %s - You win!! Yay!", userTwoChoice, userOneChoice);
+            } else if (userTwoChoice.equalsIgnoreCase("scissors") && userOneChoice.equalsIgnoreCase("rock")) {
+                System.out.printf("The computer picked %s and you picked %s - You win!! Yay!", userTwoChoice, userOneChoice);
             } else {
-                System.out.printf("The computer picked %s and you picked %s - You Lose! :( It's ok better luck next time!", computerChoice, userChoice);
+                System.out.printf("The computer picked %s and you picked %s - You Lose! :( It's ok better luck next time!", userTwoChoice, userOneChoice);
             }
+        }
 
+        //play again
+        // returns boolean to ask if user wants to play again
+        public static boolean playAgain () {
             // Give the user a way to exit the game
             System.out.println("Would you like to play again? [y/n]: ");
             String playAgain = console.nextLine();
 
-            if( playAgain.equalsIgnoreCase("n")){
+            if (playAgain.equalsIgnoreCase("n")) {
                 System.out.println("Thanks for playing! Goodbye!");
-                exit = true;
-            }else{
-                exit = false;
+                return true;
+            } else {
+                return false;
             }
+        }
 
 
-        } while (!exit);
-
-
-    }// this closes the main method
-    // grab the user choice
 }// this closes the class
+
